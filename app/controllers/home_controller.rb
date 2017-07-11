1f666f5faa48cecf1e5768c463b9eb7e4399c9e9
recs7168 <recs7168@gmail.com>
2017-07-11 11:03:18 -0400

    get_cancellable copied to home controller for shippers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,6 @@ class HomeController < ApplicationController
 	def home
 		@items_to_reorder = Item.need_reorder.alphabetical.to_a
 		@orders = Order.chronological.to_a.select! { |o| (o.order_items.map { |oi| oi.shipped_on }).include?(nil) }
-		
 		# @popular_items = 
 		# @discounted_items = 
 		# @recently_discounted_item = @discounted_items. order. last
@@ -23,5 +22,20 @@ class HomeController < ApplicationController
 	def sandbox
 		
 	end
+
+	private
+	def get_cancellable(order)
+		unless order.user_id == current_user.id || current_user.role == 'admin'
+			return false
+		end
+		
+		for oi in order.order_items
+			if oi.shipped_on != nil
+				return false
+			end
+		end
+		return true
+	end
+	helper_method :get_cancellable
   
 end
